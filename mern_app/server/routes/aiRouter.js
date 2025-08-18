f942959e02e2dbe76a0d659e87fcf81f297a88ae
Ranjith Muniyappan <ranjit.codes@gmail.com>
2025-08-18 22:26:45 +0530

    Image validator api has been tested

--- mern_app/server/routes/aiRouter.js
+++ mern_app/server/routes/aiRouter.js
@@ -2,7 +2,18 @@ const express = require("express")
 const { validateImage } = require("../controllers/aiController")
 const aiRouter = express.Router()
 
-aiRouter.route("/").post(validateImage)
+
+const multer = require("multer");
+
+// Store files in memory (not on disk)
+const storage = multer.memoryStorage();
+
+// Initialize multer
+const upload = multer({ storage: storage });
+
+
+
+aiRouter.route("/").post(upload.single("file"), validateImage)
 
 module.exports = {
     aiRouter
